db0ecc9958876f20bce5e863b8972239b2413f14
岩 黄 <1034642369@qq.com>
2019-03-15 14:12:40 +0800

    1

--- Project/大炮打蚊子.c
+++ Project/大炮打蚊子.c
@@ -1,25 +1,41 @@
 #include<stdio.h>
 int main() {
-	char B[21][21];
+	char B[21][21],tempc;
 	int L[21][21];
 	int m, n, k;
 	scanf("%d%d", &n, &m);
 	for (int i = 0; i < n; i++) {
+		getchar();
 		for (int j = 0; j < m; j++) {
-			scanf("%c", &B[j][i]);
-			if (B[j][i] == '0') {
-				L[j][i] = 3;
+			scanf("%c", &tempc);
+			if (tempc == '\n')continue;
+			B[i][j]= tempc;
+			if (B[i][j] == '0') {
+				L[i][j] = 0;
 			}
-			else if (B[j][i] == '#') {
-				L[j][i] = 2;
+			else if (B[i][j] == '#') {
+				L[i][j] = 2;
 			}
+			else if (B[i][j] == '\n')L[j][i] = 4;
 		}
 	}
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j < m; j++) { printf("%d ", B[i][j]); }
+		printf("\n");
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) { printf("%d ", L[i][j]); }
+		printf("\n");
+	}
+	printf("\n");
 	scanf("%d", &k);
 	int kn[401], km[401];
 	int ktemp = k;
 	for (int i = 0; i < k; i++) {
-		scanf("%d%d", &km[i], &kn[i]);
+		scanf("%d %d", &kn[i], &km[i]);
+	}
+	for (int i = 0; i < k; i++) {
+		printf("%d %d\n", kn[i], km[i]);
 	}
 	for (int i = 0; i < k; i++) {
 		int dead = 0;
@@ -41,7 +57,7 @@ int main() {
 					if (L[km[i] - 1][kn[i]] == 2)L[km[i] - 1][kn[i]]--;
 					if (L[km[i]][kn[i]+1] == 2)L[km[i]][kn[i]+1]--;
 					if (L[km[i]][kn[i]-1] == 2)L[km[i]][kn[i]-1]--;
-					printf("%d", dead);
+					printf("%d\n", dead);
 			ktemp--;
 		}
 	}
